641db64c1b16dcdbf3e645d5d2e28f7a2af811d2
Kaushik M K <mkkaushiksjce@gmail.com>
2017-06-15 16:26:54 +0530

    pagination parameters ui completed

--- pagination_directive/app.js
+++ pagination_directive/app.js
@@ -1,5 +1,13 @@
 var App = angular.module("PaginationDirectiveApp", []);
 
-App.controller("PaginationCtrl", ['$scope', function($scope){
-    
+App.controller("PaginationCtrl", ['$scope', function ($scope) {
+    $scope.range = 10;
+    $scope.min = 0;
+    $scope.max = 100;
+
+    $scope.verifyNumber = function($event){
+        if($event.target.value < 0){
+            $event.target.value  = -1 * $event.target.value;
+        }
+    }
 }]);
